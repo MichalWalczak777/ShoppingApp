fa660fdf7fc45e8c896b36bb736236ff641075c2
Michal Walczak <michalwalczak777@gmail.com>
2021-02-28 21:36:20 +0100

    Create website footer

--- shopping/src/components/generalUI/footer/Footer.tsx
+++ shopping/src/components/generalUI/footer/Footer.tsx
@@ -1,12 +1,38 @@
+import { AccordionSummary, AccordionDetails, Accordion, Typography } from "@material-ui/core";
 import React from "react";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+
 
 const Footer = () => {
     
+    const footerData = [{summary: "kontakt", details:[" tel. (12) 333 23 11", "sklep@bestshopping.com", <a>formularz kontaktowy</a>]},
+                        {summary: "informacje", details: ["O nas", "Nasze salony", "Polityka prywatności", "Pracuj w Best Shopping!"]},
+                        {summary: "obsługa klienta", details: ["Dostawa", "Formy płatności", "Czas realizacji zamównienia", "Regulamin", "Zwroty i reklamacje"]}];
     return (
         <footer>
-            <p>footer</p>
+            {footerData?.map(data => 
+                    <Accordion key={"accordion" + data.summary}>
+                        <AccordionSummary>
+                            {data.summary}
+                            <ExpandMoreIcon/>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <ul>
+                                {data.details?.map(dataDetails=><li key={"accordion details" + dataDetails}>{dataDetails}</li>)}
+                            </ul>
+                        </AccordionDetails>
+                    </Accordion>)}
+            <div>
+                <FacebookIcon/>
+                <InstagramIcon/>
+                <YouTubeIcon/>
+            </div>
         </footer>
     );
 }
+
 
 export default Footer;
