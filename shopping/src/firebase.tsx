b6db20cf1a5e58b76385ecf13c71da7b71d87778
Michal Walczak <michalwalczak777@gmail.com>
2021-03-11 22:52:46 +0100

    Create firebase project and import it to the application

---
+++ shopping/src/firebase.tsx
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+import "firebase/auth";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBeIZHPyby8PTGk4QXVJzaPMf8m2VMraWE",
+  authDomain: "ecommerce-clothes-e0ba2.firebaseapp.com",
+  projectId: "ecommerce-clothes-e0ba2",
+  storageBucket: "ecommerce-clothes-e0ba2.appspot.com",
+  messagingSenderId: "596515683215",
+  appId: "1:596515683215:web:0e54cefd319a591efa29fa"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
