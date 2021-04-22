ad29f079c80e64cb8b924df527fe0ea240362d42
Michal Walczak <michalwalczak777@gmail.com>
2021-04-22 23:05:08 +0200

    Create a basic version of a drawer menu

--- shopping/src/components/generalUI/header/Header.tsx
+++ shopping/src/components/generalUI/header/Header.tsx
@@ -1,36 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { AppBar,
     makeStyles,
     Toolbar,
     IconButton,
-    Menu } from "@material-ui/core";
+    Menu, 
+    Drawer,
+    Button} from "@material-ui/core";
 
 import MenuIcon from "@material-ui/icons/Menu";
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import CloseIcon from '@material-ui/icons/Close';
 import { Link } from "react-router-dom";
+
+
+const useStyles = makeStyles(() => ({
+    toolbar: {
+      display: "flex",
+      justifyContent: "space-between",
+    }
+}));
 
 const Header = () => {
 
+    const {toolbar} = useStyles();
+    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
-    const useStyles = makeStyles(() => ({
-        toolbar: {
-          display: "flex",
-          justifyContent: "space-between",
-        }
-    }));
-
-      const {toolbar} = useStyles();
+    const handleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
 
     const hamburgerMenu = () => {
         return (
-            <IconButton>
-              <MenuIcon />
+            <IconButton onClick={handleMenu}>
+              <MenuIcon/>
             </IconButton>
         )
     }
     const navigationIcons = [{icon: <AccountCircleIcon/>, name: "account", link: "/accountDetails"}, {icon: <SearchIcon/>, name: "search",  link: "/"}, {icon: <ShoppingCartIcon/>, name: "basket",  link: "/basket"}];
+    const drawerMenuOptions = [{name: "ONA", link: "/"}, {name: "ON", link: "/"}, {name: "DZIECKO", link: "/"}]
 
     const navigationMenu = () => {
         return (
@@ -41,9 +50,16 @@ const Header = () => {
                 </div>
             </Toolbar>)
     }
+
     return (
         <header className="header-appHeader">
             <AppBar>{navigationMenu()}</AppBar>
+            <Drawer anchor="left" open={isMenuOpen} onClose={() => setIsMenuOpen(false)}>
+                <p className="header-drawerMenuClose"><IconButton onClick={handleMenu}><CloseIcon/></IconButton></p>
+                <ul className="header-drawerMenu">
+                    {drawerMenuOptions?.map(option => <li key={option.name}><button className="header-drawerMenuOption" onClick={handleMenu}><Link to={option.link}>{option.name}</Link></button></li> )}
+                </ul>
+            </Drawer>
         </header>
     );
 }
