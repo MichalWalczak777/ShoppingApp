59708620f0efea3168a7b3a8328285bd87f4cc5c
Michal Walczak <michalwalczak777@gmail.com>
2022-04-04 23:18:02 +0200

    Add responsive styling to the empty basket view

--- shopping/src/components/Shopping/shoppingBasket/ShoppingBasket.tsx
+++ shopping/src/components/Shopping/shoppingBasket/ShoppingBasket.tsx
@@ -28,6 +28,47 @@ const ShoppingBasket = () => {
     },[clothes]);
 
 
+
+    const EmptyBasket = () => {
+        return (
+            <div className='shoppingBasket-empty'>
+                <div className = 'shoppingBasket-contentEmpty'>
+                    <h2 className ='shoppingBasket-headerEmpty'><ShoppingCartIcon className ='shoppingBasket-basketIcon'></ShoppingCartIcon> Koszyk jest pusty. </h2>
+                    <p> Wybierz coś dla siebie z naszej oferty </p>
+                    <Button className = 'shoppingBasket-button shoppingBasket-materialComponent' variant="outlined" color="primary" component={Link} to="/"> STRONA GŁÓWNA </Button>
+                    <p> Zaloguj się lub zarejestruj, aby otrzymywać rabaty i mieć dostęp do koszyka z dowolnego urządzenia </p>
+                    {!currentUser && <Button className = 'shoppingBasket-button shoppingBasket-materialComponent' variant="contained" color="primary" component={Link} to="/accountDetails"> ZALOGUJ SIĘ </Button>}
+                </div> 
+            </div>
+        )
+    }
+
+    const FilledBasket = () => {
+        return (
+        <div className='shoppingBasket-filled'>
+            <div className='shoppingBasket-contentFilled'>
+                <h2 className = 'shoppingBasket-mainHeader'>Twój koszyk</h2>
+                <div className='shoppingBasket-basketElementsWrapper'>                        
+                    {clothes?.map((product, i) => <BasketElement sizedProduct={product} key={product.name+i}></BasketElement>)}
+                </div>
+            </div>
+            <div className='shoppingBasket-summary'>
+                <div className='shoppingBasket-summaryContentWrapper'>
+                    <h2 className='shoppingBasket-summaryHeader'>Podsumowanie</h2>
+                    <div className="shoppingBasket-summaryContent">
+                        <div className='shoppingBasket-summaryValuesWrapper'>
+                            <div className='shoppingBasket-summaryValue'><p>Wartość koszyka: </p><p>{basketValue.toFixed(2) + " PLN"}</p></div>
+                            <div className='shoppingBasket-summaryValue'><p>Koszty wysyłki: </p><p>{displayDeliveryCost().toFixed(2) + " PLN"}</p></div>
+                        </div>
+                        <div className='shoppingBasket-summaryTotal shoppingBasket-summaryValue'><p>Łącznie do zapłaty: </p><p>{CalculateFinalPrice().toFixed(2) + " PLN"}</p></div>
+                        <Button className = 'shoppingBasket-button shoppingBasket-materialComponent' variant="contained" color="primary"> IDŹ DO KASY </Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        )
+    }
+
     const CalculateBasketValue = (): void => {
         let summaryProductsValue: number = 0;
         for (let i = 0; i < clothes.length; i++){
@@ -40,34 +81,9 @@ const ShoppingBasket = () => {
     const displayDeliveryCost = (): number => basketValue !== 0 ? deliveryCost: 0;
 
     return (
-        <div className='shoppingBasket'>
-            <div className='shoppingBasket-content'>
-                {clothes.length !== 0 && <h2 className = 'shoppingBasket-mainHeader'>Twój koszyk</h2>}
-                {clothes.length === 0 && <div className='shoppingBasket-empty'> 
-                                                <h2 className ='shoppingBasket-headerEmpty'><ShoppingCartIcon className ='shoppingBasket-basketIcon'></ShoppingCartIcon> Koszyk jest pusty. </h2>
-                                                <p> Wybierz coś dla siebie z naszej oferty </p>
-                                                <Button className = 'shoppingBasket-button shoppingBasket-materialComponent' variant="outlined" color="primary" component={Link} to="/"> STRONA GŁÓWNA </Button>
-                                                <p> Zaloguj się lub zarejestruj, aby otrzymywać rabaty i mieć dostęp do koszyka z dowolnego urządzenia </p>
-                                                {!currentUser && <Button className = 'shoppingBasket-button shoppingBasket-materialComponent' variant="contained" color="primary" component={Link} to="/accountDetails"> ZALOGUJ SIĘ </Button>}
-                                        </div>}
-                <div className='shoppingBasket-basketElementsWrapper'>                        
-                    {clothes?.map((product, i) => <BasketElement sizedProduct={product} key={product.name+i}></BasketElement>)}
-                </div>
-            </div>
-            {clothes.length !== 0 &&<div className='shoppingBasket-summary'>
-                                        <div className='shoppingBasket-summaryContentWrapper'>
-                                            <h2 className='shoppingBasket-summaryHeader'>Podsumowanie</h2>
-                                            <div className="shoppingBasket-summaryContent">
-                                                <div className='shoppingBasket-summaryValuesWrapper'>
-                                                    <div className='shoppingBasket-summaryValue'><p>Wartość koszyka: </p><p>{basketValue.toFixed(2) + " PLN"}</p></div>
-                                                    <div className='shoppingBasket-summaryValue'><p>Koszty wysyłki: </p><p>{displayDeliveryCost().toFixed(2) + " PLN"}</p></div>
-                                                </div>
-                                                <div className='shoppingBasket-summaryTotal shoppingBasket-summaryValue'><p>Łącznie do zapłaty: </p><p>{CalculateFinalPrice().toFixed(2) + " PLN"}</p></div>
-                                                <Button className = 'shoppingBasket-button shoppingBasket-materialComponent' variant="contained" color="primary"> IDŹ DO KASY </Button>
-                                            </div>
-                                        </div>
-                                    </div>}
-        </div>
+        <>
+            {clothes.length === 0 ? <EmptyBasket/> : <FilledBasket/>}
+        </>
     );
 }
 
